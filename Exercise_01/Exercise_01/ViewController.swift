e70ae3aea86f12d8085ef524de58d940bc69925f
JilGC <105502236+JilGC@users.noreply.github.com>
2022-11-23 22:36:01 -0500

    Last update

--- Exercise_01/Exercise_01/ViewController.swift
+++ Exercise_01/Exercise_01/ViewController.swift
@@ -12,6 +12,9 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     let productList = ["Endgame", "Ender's Game", "Zathura", "Ra-One"]
     let priceList = ["1997", "2000", "2004", "2010"]
     let images = ["EndGame", "Ender's_Game", "Zathura", "Ra.One"]
+    
+    let detail_01 = ["Kevin Feige", "Gigi Pritzker", "William Teitler", "Gauri Khan"]
+    let detail_02 = ["Action/Sci-fi", "Sci-fi/Action", "Adventure/Sci-fi", "Sci-fi/Action "]
     // outlets
     
     override func viewDidLoad() {
@@ -38,8 +41,8 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
        let detailVC:ProductDetailViewController = self.storyboard?.instantiateViewController(withIdentifier: "ProductDetailViewController") as! ProductDetailViewController
       // assign the values to the local variable declared in ProductDetailViewController Class
-       detailVC.productName = productList[indexPath.row]
-       detailVC.productPrice = priceList[indexPath.row]
+       detailVC.productName = detail_01[indexPath.row]
+       detailVC.productPrice = detail_02[indexPath.row]
        detailVC.productImage = UIImage(named: images[indexPath.row] + ".jpg" )!
        // make it navigate to ProductDetailViewController
        self.navigationController?.pushViewController(detailVC, animated: true)
